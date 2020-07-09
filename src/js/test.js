0312f9db6db936fa1211d39c8689999ae3fda3d7
kana <tailei.yang@bangtuike.com>
2020-07-09 17:30:13 +0800

    新闻之前

---
+++ src/js/test.js
@@ -0,0 +1,18 @@
+import duang from "2041.js"
+(async () => {
+    for (let item in obj) {
+        if (item.prop) {
+            try {
+                await duang(function () {
+                    // 
+                })
+            } catch (e) {
+                // fuck
+                // shit
+            }
+        }
+    }
+})
+
+$time_before_noon
+
